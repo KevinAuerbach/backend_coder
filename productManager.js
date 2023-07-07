0238256befd08fc1b24d19983149c80a21f6f124
Kevin Auerbach <auerbachkevin123@gmail.com>
2023-07-07 20:10:20 -0300

    desafio1

---
+++ productManager.js
@@ -0,0 +1,46 @@
+class ProductManager {
+    constructor() {
+        this.products = []
+        }
+
+    addProduct (title, description, price, thumbnail, code, stock) {
+        let max = 0
+        this.products.forEach((product) => {
+            max = product.id > max && product.id
+        })
+        let id = max + 1
+
+        const newProduct = {id, title, description, price, thumbnail, code, stock}
+        let productFound = this.products.find((product) => product.code)
+        if(productFound) {
+            console.log("A product with that code already exists")
+        }else {
+            this.products.push(newProduct) 
+            console.log("Product added!")  
+        }       
+    }
+    
+    getProducts () {
+         console.log(this.products)
+    }
+
+    getProductById (id) {
+        let idFound = this.products.find((product) => product.id == id)
+        
+        if(idFound) {
+            console.log(idFound)
+        }else {
+            console.log("Product not found!")
+        }
+    }
+}
+
+
+    let kevin = new ProductManager
+    kevin.getProducts()
+    kevin.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25)
+    kevin.getProducts()
+    kevin.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25)
+    kevin.getProductById(1)
+    kevin.getProductById(4)
+    
